da4d17ccaa5022913e25775c3827b930617e4cab
Elyor Shodiyorov <elyor.sh.99@mail.ru>
2025-02-15 22:38:52 +0500

    Mobx wrapper for Next.js App Router

--- packages/mobx-next/webpack.config.js
+++ packages/mobx-next/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         filename: "index.js",
         path: path.resolve(__dirname, 'dist'),
         libraryTarget: "umd",
+        globalObject: "this",
         clean: true
     },
     resolve: {
